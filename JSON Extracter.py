8bbedc4552e21f0cdc43ccfd65d306836e933b3b
Ed-Stout <ed.stout96@googlemail.com>
2026-08-10 23:18:00 +0100

    Sorting JSON extraction issues. Date timestamp issue resolved - it works!

---
+++ JSON Extracter.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+speeches = pd.read_csv(r"G:\My Drive\Birkbeck\Project\Hansard\hansard-speeches-2016.csv", dtype=str) #dtype = str so ids match
+speaker_roles = pd.read_json(r"G:\My Drive\Birkbeck\Project\Hansard\parliamentary_roles.json")
+
+speaker_roles = speaker_roles.explode("parliamentary_posts") #everyone with multiple roles gets a new row for each role
+speaker_roles["role_name"] = speaker_roles["parliamentary_posts"].str["parl_post_name"] #create dictionary on the parl_post_name
+
+speaker_roles["mnis_id"] = speaker_roles["mnis_id"].astype(str) #make sure mnis_id is a string so it matches the speeches df
+speaker_roles["date"] = speaker_roles["date"].astype(str).str[:10] #make sure date is a string so it matches the speeches df, keep only date and not time
+
+speaker_roles = speaker_roles[["mnis_id", "date", "role_name"]]
+
+print(speaker_roles.sample(5))
+#print(speaker_roles.head())
--- Text Extracter.py
+++ Text Extracter.py
@@ -2,9 +2,17 @@ import pandas as pd
 
 input_csv = r"G:\My Drive\Birkbeck\Project\Hansard\hansard-speeches-v310.csv"
 
-all_speeches = pd.read_csv(input_csv)
-all_speeches_2016 = all_speeches[all_speeches["year"] == 2016] #use only 2016 data to begin with
+df_chunk = []
+chunk_cnt = 0
+
+for chunk in pd.read_csv(input_csv, chunksize=100000, dtype=str):
+    df_chunk.append(chunk[chunk["year"] == "2016"])  # Filter for year 2016
+    chunk_cnt += 1
+    print("chunk", chunk_cnt, "done")
+
+all_speeches_2016 = pd.concat(df_chunk)#use only 2016 data to begin with
 all_speeches_2016.to_csv(r"G:\My Drive\Birkbeck\Project\Hansard\hansard-speeches-2016.csv", index=False)
 
 #print(f"Number of speeches in 2016: {len(all_speeches_2016)}")
 print(all_speeches_2016["date"].min(), all_speeches_2016["date"].max())
+print(f"kept {len(all_speeches_2016)} speeches from 2016")
