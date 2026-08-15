9d3283e13937f216479c6fd6db5e967379f789d9
Ed-Stout <ed.stout96@googlemail.com>
2026-08-15 16:29:35 +0100

    Finding titles for all JSON files. A decision needs to be made on how many to use for analysis as there are hundreds

--- Role Counter.py
+++ Role Counter.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-gov = pd.read_json(r"G:\My Drive\Birkbeck\Project\Hansard\parliamentary_roles.json")
+gov = pd.read_json(r"G:\My Drive\Birkbeck\Project\Hansard\government_roles.json")
 
-gov = gov.explode("parliamentary_posts")
-gov["role_name"] = gov["parliamentary_posts"].str["parl_post_name"]
+gov = gov.explode("government_posts")
+gov["role_name"] = gov["government_posts"].str["gov_post_name"]
 gov["date"] = gov["date"].astype(str).str[:10]
 
-counts = gov["role_name"].value_counts()
+counts = gov["role_name"].value_counts().head(30)
 
 print(len(counts), "distinct roles\n")
 print(counts.to_string())
