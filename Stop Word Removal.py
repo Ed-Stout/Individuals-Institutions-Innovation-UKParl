cbd13bdbfdf9901f99b0d35292745d582f914cda
Ed-Stout <ed.stout96@googlemail.com>
2026-08-16 18:09:16 +0100

    Debugging worked. Saving to local area. Assessing impact of spacy stop word removal on most common words

--- Stop Word Removal.py
+++ Stop Word Removal.py
@@ -7,27 +7,40 @@ speeches = pd.read_csv(r"G:\My Drive\Birkbeck\Project\Hansard\Hansard_Dataset_to
 pre_stops = [str(t).split() for t in speeches['tokens'].fillna('')] #adds empty string to empty cells
 
 #==========Count words==============
-def word_cnt(token_list):
-    all_words = ' '.join(speeches['tokens']).split()
-    counts = Counter(all_words)
+def word_cnt(token_list, output_csv):
+    counts = Counter(word for tokens in token_list for word in tokens)
 
-    print("total tokens: ", counts.values())
+    print("total tokens: ", sum(counts.values()))
     print("Unique tokens: ", len(counts))
 
-    print("Most common words")
-    for word, n in counts.most_common(150): #most common words
-        print(word, n)
-
     doc_freq = Counter() #document frequency
-    for tokens in token_list:
-        doc_freq.update(set(tokens.split())) #changes to set
+    for token in token_list:
+        doc_freq.update(set(token)) #changes to set
 
-    print("Appears in most docs")
-    for word, n in doc_freq.most_common(75):
-        print(word, round(n / len(list) * 100, 1), '%') #shows as %
+    #print("Most common words")
+    #for word, n in counts.most_common(250): #most common words
+    #    print(word, n)
+    
+    cnt_table = pd.DataFrame(counts.most_common(250), columns=['token', 'count'])
+
+    #print("Appears in most docs")
+    #for word, n in doc_freq.most_common(75):
+    #    print(word, round(n / len(token_list) * 100, 1), '%') #shows as %
+    
+    doc_pcts = []
+    for token in cnt_table['token']:
+        pct = round(doc_freq[token] / len(token_list) * 100, 1)
+        doc_pcts.append(pct)
+
+    cnt_table['doc_pct'] = doc_pcts
+    cnt_table.to_csv(output_csv, index=False, encoding='utf-8')
+
+    print("Sample: ", cnt_table.sample(20))
+    print("Head: ", cnt_table.head(20))
+    print("Counts saved in: ", output_csv)
         
 print("Word counts before stopword removal")
-word_cnt(pre_stops)
+word_cnt(pre_stops, 'pre_word_stop_frequencies.csv')
 
 #=======Stop word removal=======
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner']) #parser and ner 
@@ -39,4 +52,6 @@ for tokens in pre_stops:
     post_stops.append(removed)
 
 print("Word counts after stopword removal")
-word_cnt(post_stops)
+word_cnt(post_stops, 'post_word_stop_frequencies.csv')
+
+
